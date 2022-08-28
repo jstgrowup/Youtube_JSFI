8f2df80d0baf3580c4fd04471e6c030dbf58e831
jstgrowup <deysubham999@gmail.com>
2022-08-28 11:23:02 +0530

    added

--- Coding/first.js
+++ Coding/first.js
@@ -16,5 +16,62 @@
 // so state management react gives us a hook called usestate const [state,setstate]=useState() hsere the setstae is dispatcher/updatder so anything that needs to get updated needs to go through the dispatcher function and the current state should not be mutated
 // what are prop so react provides you the feacture of cerating you oun custom component because of its declarative nature so you can end up writing <Button label="hello"/> so here you are giving the button can be passed as a property  both are essentially you can catch the property for example function Button(props){console.log(props)} props are readonly so if you pass the state down to the child than it will act like a prop so here label becomes the property and in this case count pass down as a property
 
-console.log(undefined == null);
-console.log(undefined === null);
+// DIFF BETWEEN NULL AND UNDEFINED
+// var x=null
+// // console.log(x);
+// console.log(undefined==null);
+// console.log(undefined===null);
+
+// ARITHMATIC OPERATORS
+// comparision,logical,conditional
+// console.log(3 + 4 * 5);
+
+// 20+3
+// console.log(4 * 3 ** 2);
+// 9*4
+
+// console.log((a = b = 5));
+
+// precedence
+// data&&globaldata&&predencedata
+// console.log((5 && 2 && 0) || (2 && 3) || (15 && 21));
+
+// 0 || false || 21
+// console.log((5 && 2 && 0) || 2 == 3 || (15 && 21));
+
+// FUNCTIONS
+// function subham(arg1, arg2, arg3, arg4) {
+//   console.log(arguments);
+// }
+// subham(1, 2, 3, 10);
+
+// SCOPES
+// global scope , local scope and block scope
+// var x = 100;
+// function name() {
+//   console.log(y);
+//   let y = 100;
+//   //   let ,const
+// }
+// name();
+// console.log(y);
+// block {}
+// let can be reassigned but not re declared
+// var you can do reassignment as well as redeclartation
+// var x = 10;
+// var x = 20;
+// x = 80;
+// let x = 10;
+// x = 20;
+// let x=100
+// in const you cant redeclare as well as you cannot re assign
+// const x = 10;
+
+// x = 100;
+// console.log(x);
+function text() {
+  console.log(x);
+  let x = 0;
+}
+text();
+// temporal dead zone

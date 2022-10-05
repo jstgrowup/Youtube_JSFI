fc59db0ebcf8ad9786a45c1a5dd73c5636a1bc0e
jstgrowup <deysubham999@gmail.com>
2022-10-05 18:25:41 +0530

    problems in express-valiadator

--- Routing/routes/users.route.js
+++ Routing/routes/users.route.js
@@ -5,17 +5,26 @@ users.get("/", (req, res) => {
   res.send("users home route");
 });
 
-users.post("/post",
-  [body('name').isLength({ min: 3 }),
-  body('email').isEmail(),
-  body('password').isLength({ min: 5 })],
+users.post("/",
+  body('id').isNumeric().withMessage('is is not a number'),
+  body('first_name').isString().isLength({ min: 3, max: 20 }).withMessage('first name must be from 8 to 20 chars'),
+  body('last_name').isString().isLength({ min: 3, max: 20 }).withMessage('first name must be from 8 to 20 chars'),
+  body('dob').isDate(),
+  body('gender').isString(),
+  body('email').isEmail().withMessage('Incorrect Email').bail()
+  ,
+  body('password').isLength({ min: 8 }).withMessage('password must be between 8 to 12 chars.').bail(),
+
   (req, res) => {
+    // Finds the validation errors in this request and wraps them in an object with handy functions
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    res.send("users created")
+
+    res.send(req.body)
   },
+
 );
 
 

470292dff353b244793a3334ccd376b7b6594793
jstgrowup <deysubham999@gmail.com>
2022-10-10 21:03:44 +0530

    added function Currying

--- Coding/Vite/src/Rayta/store.js
+++
@@ -1,7 +0,0 @@
-// import { legacy_createStore, combineReducers, applyMiddleware } from "redux";
-// import { usersReducers } from "./Users.reducer";
-// import thunk from "redux-thunk";
-// const rootreducers = combineReducers({
-//   allusers: usersReducers,
-// });
-// export const store = legacy_createStore(rootreducers, applyMiddleware(thunk));

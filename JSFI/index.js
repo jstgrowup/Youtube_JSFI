1e0c94b9dcd712eeb5ef8cf9aafef1364d5e896b
jstgrowup <deysubham999@gmail.com>
2022-11-05 12:54:48 +0530

    array destructuring

--- JSFI/index.js
+++ JSFI/index.js
@@ -678,7 +678,12 @@
 
 // console.log(x);
 // console.log(y);
-///////////////////////////////////////////////// 
+///////////////////////////////////////////////// ARRAY DESTRUCTURING
+// let arr = [1, 2, 3, 4, 5, 6]
+// let [x,y,z] = arr
+// console.log(x);
+// console.log(y);
+// console.log(z);
 
 
 

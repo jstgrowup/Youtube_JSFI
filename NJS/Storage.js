57de4e17a7cf3fb33e96e5a53f42a318feebfd18
jstgrowup <deysubham999@gmail.com>
2022-12-28 14:30:06 +0530

    HTML 5

---
+++ NJS/Storage.js
@@ -0,0 +1,4 @@
+// LS , COOKIES , SESSIONS
+// storage are key value pairs of strings where the browsers are used to store some values 
+// session storage are the storage which is used to store something till that particular session (till that particular session) session storage data is not sent to the server 
+// all these storages have the same origin policy 

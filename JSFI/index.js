225b93d8691e45ea18c7eead3a2382fe5e7d4cbc
jstgrowup <deysubham999@gmail.com>
2023-09-17 12:29:35 +0530

    fixed

--- JSFI/index.js
+++ JSFI/index.js
@@ -982,7 +982,23 @@
 // buf.write(str)
 // console.log('buf:length', buf.length)
 // console.log('buf:', buf)
-
-
+setImmediate(() => {
+    console.log("1st Immediate");
+  });
+  
+  setImmediate(() => {
+    console.log("2nd Immediate");
+  });
+  
+  process.nextTick(() => {
+    console.log("1st Process");
+  });
+  
+  process.nextTick(() => {
+    console.log("2nd Process");
+  });
+  
+  // First event queue ends here
+  console.log("Program Started");
 
 

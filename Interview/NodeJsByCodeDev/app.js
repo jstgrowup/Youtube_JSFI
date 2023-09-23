6eadeb238565985fded94049ae1d68aa558e55fc
jstgrowup <deysubham999@gmail.com>
2023-09-23 20:36:11 +0530

    .

---
+++ Interview/NodeJsByCodeDev/app.js
@@ -0,0 +1,12 @@
+// const http = require("http");
+// const fs = require("fs/promises");
+// const server = http.createServer(async (req, res) => {
+//   const contentBuffer = await fs.readFile(__dirname);
+//   res.statusCode = 200;
+//   res.setHeader("Content-Type", "text/plain");
+//   res.end(contentBuffer.toString("utf-8"));
+// });
+// server.listen(8000, () => {
+//   console.log(`server has started`);
+// });
+
